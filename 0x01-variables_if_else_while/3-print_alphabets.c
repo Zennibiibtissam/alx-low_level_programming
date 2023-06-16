ad29b46e7400d29e124ef931ee1fa8d1ba2bc708
ibtissam zennibi <ibtissamzennibii@gmail.com>
2023-06-16 14:40:23 -0700

    UPPERCASE alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <string.h>
+#include <ctype.h>
 /**
 * main - prints the alphabet in lowercase
 *
@@ -12,6 +12,12 @@ int i = 0;
 while (i < 26)
 {
 putchar(alphabet[i]);
+i++;
+}
+i = 0;
+while (i < 26)
+{
+putchar(toupper(alphabet[i]));
 i++;
 }
 putchar('\n');
