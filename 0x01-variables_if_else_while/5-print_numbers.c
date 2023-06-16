bc8d3bf8b57489de9ad7ac64c1823e9c93810324
ibtissam zennibi <ibtissamzennibii@gmail.com>
2023-06-16 14:48:34 -0700

    numbers from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - print 0-9
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i = 0;
+while (i < 10)
+{
+printf("%i", i);
+i++;
+}
+printf("\n");
+return (0);
+}
