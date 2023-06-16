0e335171dd96d1f6f9776f46357d9413d08fce6b
ibtissam zennibi <ibtissamzennibii@gmail.com>
2023-06-16 15:13:36 -0700

    comb 4

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i, j, k, l;
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
+for (k = i; k < 10; k++)
+{
+if (k == i)
+{
+l = j + 1;
+}
+else
+{
+l = 0;
+}
+for (; l < 10; l++)
+{
+putchar(i + '0');
+putchar(j + '0');
+putchar(' ');
+putchar(k + '0');
+putchar(l + '0');
+if (i != 9 || j != 8 || k != 9 || l != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
