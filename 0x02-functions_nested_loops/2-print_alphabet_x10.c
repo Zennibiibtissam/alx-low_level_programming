195356ad2e29bfcf24bdadab49dd124470d55839
ibtissam zennibi <ibtissamzennibii@gmail.com>
2023-06-20 15:29:32 -0700

    alphabet in lowercase ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - prtints alphabet ten times in lowercase followed by a new line
+*
+* Return: Always 0 (Success)
+*/
+void print_alphabet_x10(void)
+{
+int i = 0;
+for (i = 0; i < 10; i++)
+{
+char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+int i = 0;
+while (i < 26)
+{
+_putchar(alphabet[i]);
+i++;
+}
+_putchar('\n');
+}
+}
