d904e481a0fdbbb7c835e665e60481154d9eb4fc
ibtissam zennibi <ibtissamzennibii@gmail.com>
2023-07-03 14:48:32 -0700

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _memset - ironnou
  * @s: pointer to char params
@@ -7,7 +6,6 @@
  * @n: index
  * Return: *s
  */
-
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
