f056fde39c37ebc5e3c9582ef216b0efc09af74b
ibtissam zennibi <ibtissamzennibii@gmail.com>
2023-07-27 17:49:28 -0700

    maybe done

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,1 +1,12 @@
-i
+#include "lists.h"
+
+/**
+ * before_main - Function that prints a
+ * message before the main function is executed.
+ * Return: None.
+ */
+void __attribute__((constructor)) before_main()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
